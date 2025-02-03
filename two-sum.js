2d628c4c3b063583ba47bb5812e5ca19b458f59f
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-02-03 08:09:06 -0500

    enhance two-sum function documentation with detailed comments and parameter descriptions

--- two-sum.js
+++ two-sum.js
@@ -1,3 +1,26 @@
 // solve two-sum (easy)
 
-// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+    // R: Remember only 1 solution possible
+    // R: each element only used once.
+
+    // Inputs: integer[], and sum (target).
+
+    // A: Order doesn't matter.
+
+    // P:
+
+
+    // requires explicit return statement.
+};
