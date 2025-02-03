98b7b7bfd05987e81231cbd325d8f571b1702a7e
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-02-03 09:19:52 -0500

    refactor two-sum function: add helper function and enhance comments for clarity

--- two-sum.js
+++ two-sum.js
@@ -1,10 +1,11 @@
 // solve two-sum (easy)
 
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 // You can return the answer in any order.
+
+
+const helperSum = () => {};
 
 /**
  * @param {number[]} nums
@@ -15,11 +16,21 @@ const twoSum = (nums, target) => {
     // R: Remember only 1 solution possible
     // R: each element only used once.
 
+    // Ex: [2, 7, 11, 15], target = 9
+    // Ex: [1, 3, 5, 7, 9], target = 8
+
     // Inputs: integer[], and sum (target).
 
     // A: Order doesn't matter.
 
-    // P:
+    // P: use if-else blocks.
+    // P: use the Array.methods
+    // P: actually, just subtracting from the sum (requires 1 iteration O(n))
+    // P: iterate over once using each element (and subtract from the target sum)
+    // P: then call Array.method() to find if that difference exists in the array.
+    // if found, then just return that element (SINCE only 1 solution possible).
+
+
 
 
     // requires explicit return statement.
