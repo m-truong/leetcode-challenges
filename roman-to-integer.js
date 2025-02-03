c96653cc588f1b2662da7b048bc9d58de3d62852
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-02-03 13:04:42 -0500

    Update roman-to-integer.js

--- roman-to-integer.js
+++ roman-to-integer.js
@@ -9,6 +9,11 @@
 // C can be placed before D (500) and M (1000) to make 400 and 900.
 // Given a roman numeral, convert it to an integer.
 
+// constraints:
+// 1 <= s.length <= 15
+// s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+// It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
 // questions:
 
 // edge-cases: (4) and (9) are edge-cases! IV and IX.
