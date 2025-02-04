924a284cbccd98fde29d93118dc66f4012681b39
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-02-04 11:28:55 -0500

    Add longest-common-prefix.js: implement function to find the longest common prefix in an array of strings

---
+++ longest-common-prefix.js
@@ -0,0 +1,12 @@
+// problem: Write a function to FIND the longest common prefix string amongst an array[] of strings.
+// If there is no common prefix, return an empty string "".
+
+// plan:
+// create reassignable string // initialized "" empty string
+// reassign at the end of algo-logic
+
+// edge:
+// array[] of strings
+// return: string
+
+// task:
