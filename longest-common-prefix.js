75d7ab4b6678b223f2fb5252400dfbc3f379d077
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-02-04 11:35:00 -0500

    Enhance longest-common-prefix.js: add constraints and test cases for function implementation

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -1,12 +1,21 @@
 // problem: Write a function to FIND the longest common prefix string amongst an array[] of strings.
 // If there is no common prefix, return an empty string "".
 
-// plan:
-// create reassignable string // initialized "" empty string
-// reassign at the end of algo-logic
+// constraints:
+// 1 <= strs.length <= 200(elements)
+// 0 <= strs[i].length <= 200(characters)
+// strs[i] consists of only lower-case English letters.
 
 // edge:
 // array[] of strings
 // return: string
 
+// testcases:
+// input: strs = ["flower","flow","flight"] => output: "fl"
+// input: strs = ["dog","racecar","car"] => output: "" // no common prefix
+
 // task:
+
+// plan:
+// create reassignable string // initialized "" empty string
+// reassign at the end of algo-logic
