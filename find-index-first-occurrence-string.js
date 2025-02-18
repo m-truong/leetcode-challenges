10f3dddc71c3076bd16dbf8fa5cd9dfd472d73d9
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-02-18 16:04:48 -0500

    Add detailed comments and constraints for strStr function implementation

--- find-index-first-occurrence-string.js
+++ find-index-first-occurrence-string.js
@@ -1,4 +1,14 @@
-// TODO:
+// puzzle: Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Q: does only lowercase Engilish characters mean only 26chars?
+// CQ: that constraint means between 1 and 10,000 chars for both 'needle' and 'haystack' input length
+
+// inputs:
+// both must be strings
+// min-length is at least one-char length
+//
+
+// 1 <= haystack.length, needle.length <= 104
 
 
 /**
@@ -8,4 +18,8 @@
  */
 var strStr = function(haystack, needle) {
 
+    // return integer
+
+    // return either INDEX of the location of 2ndparam(string) inside the 1stparam(string)
+    //
 };
