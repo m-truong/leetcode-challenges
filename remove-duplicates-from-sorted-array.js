87150dd9de5fc18e6253fa0af1df6f9cd9ac3263
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-02-18 13:01:06 -0500

    Refine comments and plan for removeDuplicates function to clarify logic and complexity

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -8,19 +8,32 @@
 // ORIGINAL sorted ORDER kept the same
 // after removal of all duplicates ~ HOW MANY DIFF INTEGERS are there?
 
+// beware:
+// remaining elements of nums NOT Important
+// size of nums[] array IS UNIMPORTANT
 
 // complexity:
 // keep it O(n)
 
 
 // plan:
-// create hashMap of duplicates encountered
-//
+// create hashMap of each number(integer) encountered
+// if it's not already encountered ~ then add it
+// if it's encountered already ~ then remove it
+// using HashMap ~ call prototype method to get number of unique keys
+
+// using a list/array --> iterate through O(n)
+// once reached removal ---> will be O(n) since need to shift
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+    console.log('first', nums);
 
+    // NOTE: important NEW keyword
+    const uniqueNumMap = new Map();
+
+    // return HashMap's ### keys
 };
