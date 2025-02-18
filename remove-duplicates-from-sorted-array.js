9803bcd9b47db9dd1e35b1feaee4eda97d26ea66
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-02-18 12:53:26 -0500

    Add comments outlining edge cases and complexity for removeDuplicates function

--- remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array.js
@@ -2,4 +2,25 @@
 // sorted, in ASCENDING order
 // REMOVE any DUPLICATES
 
+// edge:
+// without creating new array (in-place)
+// remove from an array (appears ONCE)
+// ORIGINAL sorted ORDER kept the same
+// after removal of all duplicates ~ HOW MANY DIFF INTEGERS are there?
+
+
+// complexity:
+// keep it O(n)
+
+
 // plan:
+// create hashMap of duplicates encountered
+//
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+
+};
