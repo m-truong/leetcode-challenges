73c264a0ac1f4f5e5e767d60d824ea9ca9f32641
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-02-19 16:22:43 -0500

    Add initial implementation of invertTree function to invert a binary tree

---
+++ invert-binary-tree.js
@@ -0,0 +1,18 @@
+// task: given a binary tree's r00t, invert the tree
+// ...returnit's r00t
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+
+};
