3a2899f446902f47a6d7959cf4f561df3d4d2e07
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-02-20 12:14:26 -0500

    Enhance comments in invertTree function to clarify tree inversion logic and traversal strategy

--- invert-binary-tree.js
+++ invert-binary-tree.js
@@ -1,9 +1,16 @@
 // task: given a binary tree's r00t, invert the tree
 // ...return it's r00t
+// invert-seems to switch the pointers of left-right TreeNodes
+// keep track of the RootNode
 
 // plan:
 // sentinel-nodes?
-//
+// will have to traverse Tree until reach the leaf nodes
+// then traverse until this.left && this.right are BOTH null
+// once end of tree is reached
+//  ... will have to backtrack-> and MIRROR the left-to-right treenodes
+
+// useful to use DP or Recursion
 
 // edge:
 
