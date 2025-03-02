d7737745bb2d9bfc04e9b41708158f82d42ee5d6
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-02 16:04:10 -0500

    Update comments in valid-anagram function to clarify input constraints and plan

--- valid-anagram.js
+++ valid-anagram.js
@@ -1,10 +1,13 @@
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 // Q&A constraints:
-// 1 <= s.length, t.length <= 5 * 104
-// s and t consist of lowercase English letters.
+// 1 <= s.length, t.length <= 5 * 104 (both min-length of 1 string)
+// s and t consist of ONLY lowercase English letters.
 
 // plan:
+// count the number of charz in both string inputs
+// use a HashMap (key:values)
+//
 
 // edge:
 // can inputs be empty?
