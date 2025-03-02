214aabf3f84103f35f6618af72b2ca69f3716cbd
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-02 16:00:40 -0500

    Add initial implementation and comments for valid-anagram function

---
+++ valid-anagram.js
@@ -0,0 +1,11 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// Q&A constraints:
+//
+
+// plan:
+
+// edge:
+// can inputs be empty?
+// any length? or fixed-length?
+// return boolean
