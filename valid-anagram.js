d3590bca9c9006d52b7d531dcb4ef4bd790bb738
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-02 16:01:18 -0500

    Update comments in valid-anagram function to clarify constraints on input strings

--- valid-anagram.js
+++ valid-anagram.js
@@ -1,7 +1,8 @@
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 // Q&A constraints:
-//
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
 
 // plan:
 
