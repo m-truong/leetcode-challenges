7400924cd6ef413c21b4e156d9c18e902193e21b
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-02 16:52:45 -0500

    Refactor isAnagram function to improve character frequency counting and enhance comments for clarity

--- valid-anagram.js
+++ valid-anagram.js
@@ -22,27 +22,52 @@
 
 // ES6 arrow expression
 const isAnagram = (s, t) => {
-
     // turn the strings into arrays of 'chars'
     // then iterate thru each, and CountFrequency of each char
     // use for of loop
     // if the count of each Char is the same, then 2nd input is anagram of the 1st
-
     // remember using String.prototype.split('') String/Split
     // String split splits a string into an array by the separator
     // remember to always use the "" double-quotes as the separator
 
-    const charMap = new Map();
+    const tCharMap = new Map();
+    const sCharMap = new Map();
     const tCharArray = t.split('');
     const sCharArray = s.split('');
-
     console.log('print tCharArray', JSON.stringify(tCharArray));
     console.log('print sCharArray', JSON.stringify(sCharArray));
 
-    for (let char in tCharArray) {
-        // if the char isn't in the Map, add it
-        // if it's already in there, then uptick the frequency
-
-
+    // if the char isn't in the Map, add it
+    // if it's already in there, then uptick the frequency
+    // double-bang will force a VALUE into it's boolean type
+    // first bang (!) converts into boolean-negation, then 2nd bang (!) converts into true-boolean-value
+    for (let char of tCharArray) { // O(n)
+        if (tCharMap.has(char)) {
+            tCharMap.set(char, tCharMap.get(char) + 1);
+        } else {
+            tCharMap.set(char, 1);
+        }
     }
+
+    // now loop through the sCharArray
+    for (let char of sCharArray) { // O(n)
+        if (sCharMap.has(char)) {
+            sCharMap.set(char, sCharMap.get(char) + 1);
+        } else {
+            sCharMap.set(char, 1);
+        }
+    }
+    console.log('print charArray', JSON.stringify(tCharMap));
+    console.log('print charArray', JSON.stringify(tCharMap));
+
+    for (let key in tCharMap) {
+        if (sCharMap.has(key)) {
+            const sValue = sCharMap.get(key);
+            const tValue = tCharMap.get(key);
+            if (sValue !== tValue) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
