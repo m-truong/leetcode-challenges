bf1db65609e1bd9eb8946e767a6a1f66f3227b90
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-12 14:32:51 -0400

    Add complexity analysis to binary search algorithm documentation

--- binary-search.js
+++ binary-search.js
@@ -10,3 +10,13 @@
 // -104 < nums[i], target < 104
 // All the integers in nums are unique.
 // nums is sorted in ascending order.
+
+// Complexity Analysis
+// Let n be the size of the input array nums.
+
+// Time complexity: O(logn)
+
+// nums is divided into half each time. In the worst-case scenario, we need to cut nums until the range has no element, and it takes logarithmic time to reach this break condition.
+// Space complexity: O(1)
+
+// During the loop, we only need to record three indexes, left, right, and mid, they take constant space.
