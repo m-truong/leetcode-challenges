46509e472e4e092f3569199620b64ec9e094e2ef
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-13 14:35:10 -0400

    Implement target comparison logic in binary search algorithm

--- binary-search.js
+++ binary-search.js
@@ -54,7 +54,18 @@ var search = function(nums, target) {
     while (left <= right) {
         // Get the middle index
         let mid = Math.floor(left + (right - left) / 2);
-
+        // Case 1: If middle element is the target, return its index
+        if (nums[mid] === target) {
+            return mid;
+        }
+        // Case 2: If middle element is smaller, search the right half
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        // Case 3: If middle element is larger, search the left half
+        else {
+            right = mid - 1;
+        }
     }
 
     // If target is not found, return -1
