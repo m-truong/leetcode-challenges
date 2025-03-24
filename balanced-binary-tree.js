c3fd88393c016e73ad41c97db79f6a20829a67a0
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-03-24 13:35:51 -0400

    Add TreeNode class definition for binary tree node inputs in balanced-binary-tree.js

--- balanced-binary-tree.js
+++ balanced-binary-tree.js
@@ -7,6 +7,14 @@
 
 // psuedo
 
+/**
+ * definition of binary tree node inputs. essentially ListNode * class constructor function.
+ * function TreeNode (val, left, right) {
+ *  this.val = (val === undefined ? : 0 : val);
+ *  this.left = (val === undefined ? : 0 : val);
+ *  this.right = (val === undefined ? : 0 : val);
+ * }
+ */
 
 /**
  * @param {number[]} nums[]
