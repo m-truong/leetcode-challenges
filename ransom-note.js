707e0ad172b9fc83c381a939996d7ca70eed0cbc
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-15 20:38:39 -0400

    Refine comments in ransom-note.js to clarify problem statement and task description

--- ransom-note.js
+++ ransom-note.js
@@ -1,12 +1,15 @@
 // problem
+// return true if string param 'ransomNote' can be constructed using the letters from string param 'magazine'
+// return false if otherwise cannot be constructed
+
+// constraints:
+// note each 'char' in string-param 'magazine' only once
+// 1 <= ransomNote.length, magazine.length <= 10^5
+// ransomNote, magazine ONLY ALLOWABLE lowercase English letters
 
 // task
 
 // edge
-
-// constraints:
-// 1 <= ransomNote.length, magazine.length <= 10^5
-// ransomNote, magazine ONLY ALLOWABLE lowercase English letters
 
 /** takes 2 parameters
  * @param {string} ransomNote
