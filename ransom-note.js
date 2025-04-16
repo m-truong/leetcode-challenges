415717cc893ac035090528e3cb0f90b416181beb
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-16 15:03:08 -0400

    Refactor comments in ransom-note.js for clarity and consistency, correcting typos and enhancing structure.

--- ransom-note.js
+++ ransom-note.js
@@ -1,17 +1,22 @@
-// problem
+// brain-teaser:
 // return true if string param 'ransomNote' can be constructed using the letters from string param 'magazine'
 // return false if otherwise cannot be constructed
 
 // constraints:
-// note each 'char' in string-param 'magazine' only once
 // 1 <= ransomNote.length, magazine.length <= 10^5
 // ransomNote, magazine ONLY ALLOWABLE lowercase English letters
 
-// edge
-// can lengths of 'ransomeNote' and 'magazine' DIFFER?
+// edge questions:
+// can lengths of 'ransomNote' and 'magazine' DIFFER?
+// -> Yes
 // can we have empty string input arguments for the canConstruct function?
+// -> No (min 1 char)
+// each 'char' in string-param 'magazine' can ONLY be USED ONCE, inside 'ransomNote'
+// -> handle this scenario by ensuring that 'ransomNote' key-values is (<=) (less than or equal to) the 'magazine' key-values
 
-// questions
+// edgecases
+// if single-char, just compare if the same, then return boolean
+// saves trouble of running whole function
 
 /** takes 2 parameters
  * @param {string} ransomNote
@@ -27,11 +32,11 @@ const canConstruct = (ransomNote, magazine) => {
     // and count the #occurrences of each char
     // map them into a hashmap
 
-    // iterate over 'ransomeNote' string
+    // iterate over 'ransomNote' string
     // split the string into array of 'chars'
     // count the #occurences of each char
     // map them to a hashmap
 
-    // first check if ALL the keys of 'ransomeNote' map
+    // first check if ALL the keys of 'ransomNote' map
     // exist
 };
