48425bbc436dbfab1356f425ca4836bd1fe4a085
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-15 20:33:33 -0400

    Add constraints and parameter documentation for canConstruct function in ransom-note.js

--- ransom-note.js
+++ ransom-note.js
@@ -5,3 +5,13 @@
 // edge
 
 // constraints:
+// 1 <= ransomNote.length, magazine.length <= 10^5
+// ransomNote, magazine ONLY ALLOWABLE lowercase English letters
+
+/** takes 2 parameters
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+
+const canConstruct = (ransomNote, magazine) => {};
