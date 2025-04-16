8ebaf678c04dc72f236ee1834c90ec57831098f7
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-16 14:54:36 -0400

    Refactor comments in ransom-note.js to enhance clarity and structure for the canConstruct function

--- ransom-note.js
+++ ransom-note.js
@@ -7,9 +7,10 @@
 // 1 <= ransomNote.length, magazine.length <= 10^5
 // ransomNote, magazine ONLY ALLOWABLE lowercase English letters
 
-// task
 
 // edge
+
+// questions
 
 /** takes 2 parameters
  * @param {string} ransomNote
@@ -17,4 +18,19 @@
  * @return {boolean}
  */
 
-const canConstruct = (ransomNote, magazine) => {};
+const canConstruct = (ransomNote, magazine) => {
+    // use ES6 library on Strings built-in operations
+
+    // take the 'magazine' string and split into array of 'chars'
+    // iterate over magazine's char-array
+    // and count the #occurrences of each char
+    // map them into a hashmap
+
+    // iterate over 'ransomeNote' string
+    // split the string into array of 'chars'
+    // count the #occurences of each char
+    // map them to a hashmap
+
+    // first check if ALL the keys of 'ransomeNote' map
+    // exist
+};
