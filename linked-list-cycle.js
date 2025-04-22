15dec8a156f6c2c8682c2b26f4e21b4756cb21f9
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-22 12:02:47 -0400

    Refine comments in linked-list-cycle.js to enhance clarity on constraints, edge cases, and time complexity analysis for cycle detection

--- linked-list-cycle.js
+++ linked-list-cycle.js
@@ -12,13 +12,14 @@
 // internally, there's a 'pos' variable (either valid index or -1)
 // unsure if can use 'pos' in some way
 
-// constraints // edgecases:
+// constraints // edgecases // pseudo:
 // looks like given head, can have a 'head' ListNode that is undefined!
 // workaround for this gotcha, is to use a 'sentinel' node
 // or can just check if 'head' input is undefined, then return false
-
-// psuedo:
-
+// also can have asymptotic analysis as # listnodes that head is connected to tends towards 10,000 listnodes
+// if i'm traversing that many listnodes to determine if there's a cycle,
+// then time-complexity would be O(n) average-linear time
+// the values don't matter, but the val properties of every ListNode can be negative integers
 
 /**
  * @param
