cd4dbe2d0ef1769377896027bdc3410f815d0926
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-22 11:53:19 -0400

    Enhance comments in linked-list-cycle.js to clarify task requirements and edge cases for cycle detection

--- linked-list-cycle.js
+++ linked-list-cycle.js
@@ -6,6 +6,17 @@
  * }
  */
 
+// task:
+// need to detect if there's a cycle
+// input is ALWAYS the head ListNode
+// internally, there's a 'pos' variable (either valid index or -1)
+
+// constraints // edgecases:
+// looks like given head, can have a 'head' ListNode that is undefined!
+
+// psuedo:
+
+
 /**
  * @param
  * @param
