a013ce4a692c3dd2bf4bdc89dbecbf8417a62c80
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-22 11:57:59 -0400

    Enhance comments in linked-list-cycle.js to clarify input handling and edge cases for cycle detection

--- linked-list-cycle.js
+++ linked-list-cycle.js
@@ -8,11 +8,14 @@
 
 // task:
 // need to detect if there's a cycle
-// input is ALWAYS the head ListNode
+// input is ALWAYS the head ListNode (warning: can be undefined 'head')
 // internally, there's a 'pos' variable (either valid index or -1)
+// unsure if can use 'pos' in some way
 
 // constraints // edgecases:
 // looks like given head, can have a 'head' ListNode that is undefined!
+// workaround for this gotcha, is to use a 'sentinel' node
+// or can just check if 'head' input is undefined, then return false
 
 // psuedo:
 
