e651b868d22cb7c13f0a80660b671d9af5eec9e7
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-25 19:45:55 -0400

    Refactor comments in implement-queue-using-stacks.js to clarify the order of method calls and core functionality of MyQueue

--- implement-queue-using-stacks.js
+++ implement-queue-using-stacks.js
@@ -14,9 +14,18 @@
 // -> this means that pop() and peek() will ONLY be called with integers inside the QUEUE
 
 // edge-corner-cases (i.e. gotchas)
-// in javascript, a dynamic-array is used to implement a stack
-// so a 2 stacks would be 2 dynamic-arrays
-// stacks are (FILO) "first-in-last-out"
+// so pop() and peek() will never be called first
+// push() and empty() will be called BEFORE pop() and peek()
+//
 
 // psuedo:
 // since it's a MyQueue class,
+// in javascript, a dynamic-array is used to implement a stack
+// so a 2 stacks would be 2 dynamic-arrays
+// stacks are (FILO) "first-in-last-out"
+// if using 2 arrays, then have to move integer elements from one array to the other array
+// this'll be the core functionality
+// need to initialize 2 separate arrays within scope of the MyQueue class
+// then every method call push(), pop(), peek(), empty() will be called and mutate the
+// ... 2 internal dynamic arrays
+// maybe call one array Enqueue? other array dequeue()
