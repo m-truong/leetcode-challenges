2f22c48e5bef4f654515cf20353003d4df231279
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-25 16:08:14 -0400

    Refactor comments in implement-queue-using-stacks.js to clarify MyQueue methods and their functionality

--- implement-queue-using-stacks.js
+++ implement-queue-using-stacks.js
@@ -1,4 +1,10 @@
 // task: implement a queues using 2 stacks in JavaScript
+// the MyQueue class will need to have 4 built-in methods
+// these methods will support the standard operations of 'Queue'
+// 1] void push (int x) ~ pushes an integer to the BACK of the queue
+// 2] int pop() ~ this will return+remove the first integer in the queue
+// 3] int peek() ~ this just returns the integer value at the front
+// 4] boolean empty() ~ determines if queue IS/ISN'T empty
 
 // constraints:
 
