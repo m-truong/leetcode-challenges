89cf8e52131d49a89648ef4c6fe85a6c97073724
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-25 16:24:05 -0400

    Enhance comments in implement-queue-using-stacks.js to clarify constraints and edge cases for MyQueue implementation

--- implement-queue-using-stacks.js
+++ implement-queue-using-stacks.js
@@ -7,8 +7,16 @@
 // 4] boolean empty() ~ determines if queue IS/ISN'T empty
 
 // constraints:
-
+// the ONLY allowable values inside the QUEUE are positive integers from 1-9
+// "At Most 100 calls will be made to push, pop, peek, empty."
+// -> this means that the automated-unit-tests will BE LIMITED to 100 calls
+// "All pop, peek calls are VALID."
+// -> this means that pop() and peek() will ONLY be called with integers inside the QUEUE
 
 // edge-corner-cases (i.e. gotchas)
+// in javascript, a dynamic-array is used to implement a stack
+// so a 2 stacks would be 2 dynamic-arrays
+// stacks are (FILO) "first-in-last-out"
 
 // psuedo:
+// since it's a MyQueue class,
