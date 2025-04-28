814ed04d0177534e726f17fdfef88158e2bc9535
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-28 18:08:52 -0400

    Initialize isBadVersion function definition with parameter and return type documentation

---
+++ first-bad-version.js
@@ -0,0 +1,9 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
