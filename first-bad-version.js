f0e94b302a32505097e24aded092760ba99b22ff
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-04-29 13:53:24 -0400

    Refine comments and implement binary search logic to find the first bad version

--- first-bad-version.js
+++ first-bad-version.js
@@ -6,7 +6,8 @@
 // bad && n are both only between 1~2,147,483,647
 
 // psuedo:
-//
+// logic wiould be that every version have the bad version would be bad
+// this is cause only version get incremented from 1+
 
 /**
  * Definition for isBadVersion()
@@ -31,5 +32,15 @@ var solution = function(isBadVersion) {
         // TODO: write return function to solve.
         // is this a loop of calling isBadVersion
         // this has to be Math; use modulo and round up
+        let left = 1, right = n;
+        while (left < right) {
+            const mid = Math.floor((left + right) / 2);
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     };
 };
