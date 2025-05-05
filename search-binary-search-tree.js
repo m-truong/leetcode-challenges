a925d67cc23265d2fbedea2ec1676012d4d4f02f
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-05-05 18:51:24 -0400

    solved

--- search-binary-search-tree.js
+++ search-binary-search-tree.js
@@ -6,11 +6,23 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
 /**
  * @param {TreeNode} root
  * @param {number} val
  * @return {TreeNode}
  */
 var searchBST = function(root, val) {
-
+    let res = null;
+    const helper = (node) => {
+        if (!node) return;
+        if (node.val === val) {
+            res = node;
+            return;
+        }
+        if (val < node.val) helper(node.left)
+        if (val > node. val) helper (node.right)
+    }
+    helper(root);
+    return res;
 };
