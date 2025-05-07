2d96bcf5b29195d61830ee11431de810e897f50c
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-05-07 14:26:32 -0400

    Refactor comments and pseudo code in lowestCommonAncestor function for clarity

--- lowest-common-ancestor-binary-tree.js
+++ lowest-common-ancestor-binary-tree.js
@@ -1,3 +1,16 @@
+// constraints:
+// at least 2 treeNodes
+// all the values must be unique (no duplicates)
+// node.values are also (-) negative
+// argument 'p' will not be same as 'q'
+
+// edgecases:
+// if p or q is an ancester of the other; then just return that node
+//
+
+// pseudo:
+// how do I traverse the tree?
+
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
@@ -13,5 +26,11 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-
+    if (root.val < p.val && root.val < q.val) {
+        return lowestCommonAncestor(root.right, p, q);
+    }
+    if (root.val > p.val && root.val > q.val) {
+        return lowestCommonAncestor(root.left, p, q);
+    }
+    return root;
 };
