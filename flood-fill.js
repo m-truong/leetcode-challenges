aefed167c075fdaa44b940a3037734b0dec3c308
Michael Truong <truong.michael.2013@gmail.com>
2025-05-16 15:41:15 -0400

    Add pseudo

--- flood-fill.js
+++ flood-fill.js
@@ -1,3 +1,29 @@
+// the grid[][] is the image
+// ex: image[i][j] is the pixel value
+// ex: m by n grid
+
+// sr & sc are both given as parameters in the function itself
+// essentially starting from sr (JS integer)
+// sr = start row
+// sc = start column
+
+// goal:
+// basically, swap out the 'pixel' number w the target 'color' number primitive
+// take the sr/sc as the 'index' inside the 2D array
+
+// approach:
+// if the initial 'pixel' primitive number is different from the 'color' primitive number, then change it
+// if it's the same, then there's no work to be done for the function
+
+// if the 'pixel' IS different from the 'color' primitive, then enter MEAT of flood fill algorithm
+// first swap the initial 'pixel'
+// then check the elements before/after the initial 'pixel'
+// if they're the SAME as the initial pixel, then SWITCH the primitive
+
+// then ...
+
+// start from within and ...
+
 /**
  * @param {number[][]} image
  * @param {number} sr
@@ -6,30 +32,20 @@
  * @return {number[][]}
  */
 var floodFill = function(image, sr, sc, color) {
+  // store initial pixel number using the indices src/sc
+  // practice: storing a Value that is important to the algo!
+  const originalColorPrimitive = image[sr][sc];
 
-  // the grid[][] is the image
-  // ex: image[i][j] is the pixel value
-  // ex: m by n grid
+  // then check for edgecase if the original color is the same as the target color; and if it is, then just return the same image
+  // practice: using one line if-statement
+  if (originalColorPrimitive === color) return image;
 
-  // sr & sc are both given as parameters in the function itself
-  // essentially starting from sr (JS integer)
-  // sr = start row
-  // sc = start column
+  // store the important values like the total rows/columns of grid
+  // note: it's possible to call .length even on a 2D array
+  // imageHeight should be the number of 'nested' arrays (i.e. rows)
+  const imageHeight = image.length;
 
-  // goal:
-  // basically, swap out the 'pixel' number w the target 'color' number primitive
-  // take the sr/sc as the 'index' inside the 2D array
-
-  // approach:
-  // if the initial 'pixel' primitive number is different from the 'color' primitive number, then change it
-  // if it's the same, then there's no work to be done for the function
-
-  // if the 'pixel' IS different from the 'color' primitive, then enter MEAT of flood fill algorithm
-  // first swap the initial 'pixel'
-  // then check the elements before/after the initial 'pixel'
-  // if they're the SAME as the initial pixel, then SWITCH the primitive
-
-  // then ...
-
-  // start from within and 
+  // imageWidth should be the number of elements WITHIN a 'nested' array
+  // this is essentially the 'width' of the image (i.e. columns)
+  const imageWidth = image[0].length;
 };
