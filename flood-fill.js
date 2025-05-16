05eb0759dee5c016aa5338b54091b96ffb5da4c9
Michael Truong <truong.michael.2013@gmail.com>
2025-05-16 12:38:14 -0400

    Correct function definition

--- flood-fill.js
+++ flood-fill.js
@@ -1,6 +1,10 @@
 /**
- * @param {number[]} nums
- * @param {number} num
- * @return {number}
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
  */
-
+var floodFill = function(image, sr, sc, color) {
+    
+};
