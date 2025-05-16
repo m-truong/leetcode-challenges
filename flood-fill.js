e39ccccb94af489a47950556b2e4ae696325e3bf
Michael Truong <truong.michael.2013@gmail.com>
2025-05-16 15:22:53 -0400

    Add pseudo

--- flood-fill.js
+++ flood-fill.js
@@ -16,5 +16,20 @@ var floodFill = function(image, sr, sc, color) {
   // sr = start row
   // sc = start column
 
+  // goal:
+  // basically, swap out the 'pixel' number w the target 'color' number primitive
+  // take the sr/sc as the 'index' inside the 2D array
 
+  // approach:
+  // if the initial 'pixel' primitive number is different from the 'color' primitive number, then change it
+  // if it's the same, then there's no work to be done for the function
+
+  // if the 'pixel' IS different from the 'color' primitive, then enter MEAT of flood fill algorithm
+  // first swap the initial 'pixel'
+  // then check the elements before/after the initial 'pixel'
+  // if they're the SAME as the initial pixel, then SWITCH the primitive
+
+  // then ...
+
+  // start from within and 
 };
