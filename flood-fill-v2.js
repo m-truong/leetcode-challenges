b28ee79854d88f7d1e6cc72e87e4c20996f7c06e
Michael Truong <truong.michael.2013@gmail.com>
2025-05-17 17:52:58 -0400

    Create attempt flood-fill second algo file

---
+++ flood-fill-v2.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} twoDArrayImage
+ * @param {number} startRowIndex
+ * @param {number} startColumnIndex
+ * @param {number} targetColorNumber
+ * @return {number[][]}
+ */
+
+// takes 4 parameters
+const paintFill = (twoDArrayImage, startRowIndex, startColumnIndex, targetColorNumber) => {
+
+  // start by storing the original 'pixel's number
+  const originalPixelNumber = twoDArrayImage[startRowIndex][startColumnIndex];
+
+  // 
+
+  // return original image
+  return twoDArrayImage;
+}
