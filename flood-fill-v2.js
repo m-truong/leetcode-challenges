5a9cfda72ffd828c99e7bd4ea2993ee55e37fbb8
Michael Truong <truong.michael.2013@gmail.com>
2025-05-18 21:45:00 -0400

    Add comments and function logic

--- flood-fill-v2.js
+++ flood-fill-v2.js
@@ -12,8 +12,26 @@ const paintFill = (twoDArrayImage, startRowIndex, startColumnIndex, targetColorN
   // start by storing the original 'pixel's number
   const originalPixelNumber = twoDArrayImage[startRowIndex][startColumnIndex];
 
-  // 
+  // check edgecase for when the originalPixelNumber === targetColorNumber
+  // cause if that's the case, then there's no changes needed to be done to the original image
+  if (originalPixelNumber === targetColorNumber) return twoDArrayImage;
 
+  // i guess it's useful to store the length and width of the 2D array
+  // since 'nested' arrays are considered 'grids' in programming
+  // it's good idea to store useful values that're critical to solving the problem
+  const imageHeight = image.length;
+  const imageWidth = image[0].length;
+
+  // recursion
+  const recursiveAdjacentPixelFill = (sr, sc) => {
+
+  };
+  // base case
+  // action step
+  // recursive call
+
+  // invoke the recursive function with the startRowIndex & startColIndex
+  recursiveAdjacentPixelFill(startRowIndex, startColumnIndex);
   // return original image
   return twoDArrayImage;
 }
