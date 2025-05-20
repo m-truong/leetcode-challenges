a955e174351a5c824eab3f234a92aa5d183f9c2f
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-05-20 14:54:09 -0400

    Enhance comments in island perimeter function to clarify grid cell scenarios affecting perimeter calculation

--- island-perimeter.js
+++ island-perimeter.js
@@ -1,11 +1,15 @@
 // You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
-
 // Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
-
 // The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
 
 // approach:
 // each land cell w value 1 that is adjacent vertically/horizontally to 0 counts towards island perimeter sum
+// grid cell scenarios that sum towards perimeter:
+// 1) any 1's at the first/last index of outer array cause it touches the surrounding water
+// 2) any 1's in-between first/last index of outer array that are at beginning/end of inner array cause it touches the surrounding water
+// 3) TBD
+// 4) any 1's touching 0's left/right is a perimeter side
+// 5) any 1's touching 0' above/below is a perimeter side
 
 // constraints:
 // shape of grid is rectangular (which means grid.length doesn't have to equal grid[i].length)
@@ -20,4 +24,5 @@
  * @return {number}
  */
 
-const islandPerimeter = (grid) => {};
+const islandPerimeter = (grid) => {
+};
