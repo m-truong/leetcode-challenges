df7fa7de1fd2419c1b03d4fe0e7b71a6b95787e6
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-05-27 19:54:39 -0400

    Initialize maximum-subarray file with constraints and function signature

---
+++ maximum-subarray.js
@@ -0,0 +1,13 @@
+// constraints:
+// min length is [1]
+// cannot be empty
+// elements can be negative (-)
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+const maxSubArray = () => {
+
+}
