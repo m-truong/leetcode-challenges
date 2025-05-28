a03c837f089381347b1a7090d4869dccd5eea5be
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-05-27 20:01:48 -0400

    Add questions and pseudo code comments to maximum-subarray file for clarity

--- maximum-subarray.js
+++ maximum-subarray.js
@@ -3,11 +3,20 @@
 // cannot be empty
 // elements can be negative (-)
 
+// questions:
+// what's a subarray?
+// does it have to be contiguous?
+
+
+// pseudo:
+
+//
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
 
 const maxSubArray = () => {
-
+    console.log('printing', nums);
 }
