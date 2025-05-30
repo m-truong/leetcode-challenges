ff09f7b50873e8405f1a045ea7d796669479e3c3
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-05-30 17:04:02 -0400

    Add loop structure to maxSubArray function for iteration over elements

--- maximum-subarray-v2.js
+++ maximum-subarray-v2.js
@@ -12,4 +12,10 @@ const maxSubArray = () => {
 
     // let's assume that the very first element in the dynamic-array is the current running sum, and the maximum sum at the same time
     let currSum = nums[0], maxSum = nums [0];
+
+    // start a for-iterative loop, and since we know we want to iterate through all the elements in the array, until the end, let's have it stop at the total length of the nums[] array
+    // start from second element since assuming first element is the maxSum
+    for (let k = 1; k < nums.length; k++) {
+
+    }
 }
