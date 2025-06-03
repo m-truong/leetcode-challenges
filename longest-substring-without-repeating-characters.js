afb003a50928b38cfcaed59feeecb802842e0caf
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-06-03 14:57:06 -0400

    Add initial implementation and comments for lengthOfLongestSubstring function

---
+++ longest-substring-without-repeating-characters.js
@@ -0,0 +1,19 @@
+// constraints:
+// input is string
+// return a number
+// s.length can be 0
+// can also be digits, symbols, or spaces
+
+// pseudo:
+// intuition that core idea behind our solution is to use a sliding window approach combined with character indexing
+// This allows us to efficiently scan through the string once while keeping track of the longest valid substring we've encountered so far.
+
+// questions:
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+
+};
