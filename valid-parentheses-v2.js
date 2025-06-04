b6ca10d538c4c8d8df2a8001c4fd5629dabb3509
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-06-04 16:20:51 -0400

    Add initial implementation of validParentheses function with comments

---
+++ valid-parentheses-v2.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+const validParentheses = () => {
+    // store string length as a variable
+
+    // use a stack (dynamic array) to track order of 'open' parentheses
+
+    // iterate through string
+
+    // optional: use hashmap key-value pair to map the 'open' brace to corresponding 'closing' brace
+
+}
