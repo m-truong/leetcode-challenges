652f5e78f0c2edc2cb1c8638b5976283a7617474
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-06-04 16:23:31 -0400

    Add comments to validParentheses function for clarity on edge cases and logic

--- valid-parentheses-v2.js
+++ valid-parentheses-v2.js
@@ -4,6 +4,9 @@
  */
 
 const validParentheses = () => {
+
+    // edgecase: if it starts with a closing brace then return false
+
     // store string length as a variable
 
     // use a stack (dynamic array) to track order of 'open' parentheses
@@ -11,5 +14,11 @@ const validParentheses = () => {
     // iterate through string
 
     // optional: use hashmap key-value pair to map the 'open' brace to corresponding 'closing' brace
+
+    // everytime I encounter an open brace, add it to the stack
+
+    // except when it's a closing brace then check the hashmap, and if there's a matching brace then pop it off the stack
+
+    //
 
 }
