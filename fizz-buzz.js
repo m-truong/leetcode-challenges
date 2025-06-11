6b55b2bc401a41551ab770fc1ebf890922cc7ef2
Michael Truong <truong.michael.2013@gmail.com>
2025-06-11 10:23:52 -0400

    Solve fizzbuzz

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -6,4 +6,13 @@
  * @return {string[]}
  */
 
-const fizzBuzz = () => {}
+const fizzBuzz = () => {
+  const res = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 === 0) res.push("FizzBuzz");
+        else if (i % 3 === 0) res.push("Fizz");
+        else if (i % 5 === 0) res.push("Buzz");
+        else res.push(i.toString());
+    }
+    return res;
+}
