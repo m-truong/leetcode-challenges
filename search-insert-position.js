18f6c8d258055583c8aef1b96c5e38e4888853bb
Michael Truong <truong.michael.2013@gmail.com>
2025-06-11 13:48:12 -0400

    Add problem statement

--- search-insert-position.js
+++ search-insert-position.js
@@ -18,3 +18,11 @@
 // can have large neg. numbers
 // no dups (removes edgecase (ordered))
 // target (integer) WILL exist in nums[i] (removes EC)
+
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+const searchInsert = () => {
+
+}
