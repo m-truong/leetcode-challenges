ba2c4e9ad115691432e714a3e88fb4eff779aa34
Michael Truong <truong.michael.2013@gmail.com>
2025-06-11 14:44:25 -0400

    Comments

--- search-insert-position.js
+++ search-insert-position.js
@@ -20,9 +20,13 @@
 // target (integer) WILL exist in nums[i] (removes EC)
 
 // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-
 // You must write an algorithm with O(log n) runtime complexity.
 
+// Get length of integers array
+// divide that length by 2
+// take that as the middle point
+// compare if the target is less than or greater than half the total numbers of elements in the array
+// and then 
 const searchInsert = () => {
 
 }
