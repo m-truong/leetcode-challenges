f14f784c586d27f4b76fca3f7f47dd6086a82097
Michael Truong <truong.michael.2013@gmail.com>
2025-06-20 12:37:45 -0400

    Add comments

--- fizz-buzz-v2.js
+++ fizz-buzz-v2.js
@@ -9,4 +9,8 @@
 
 // T:
 // given a JS number primitive as input
-// divide it by 
+// divide it by 15, 5, and 3
+// then each time i divide it, i check the if the remainder is 0
+// it should be 0
+// if it is 0, then that means it's divisible by 15, 5, 3
+// so then push into my JS dynamic array the strings "Fizz", "Buzz", or "FizzBuzz"
