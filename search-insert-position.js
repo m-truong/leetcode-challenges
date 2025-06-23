5d54c32e9a336bc052554df132cef3d79e391e72
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-06-23 19:32:25 -0400

    Refactor comments for clarity and organization

--- search-insert-position.js
+++ search-insert-position.js
@@ -4,14 +4,6 @@
  * @return {}
  */
 
-// binary search
-// is always just splitting in the middle
-// and then just search the left half of a group
-// then search the rifht half
-// but we cut the search in half
-
-// q:
-
 // constraints:
 // O(logn)
 // min [1] element in array
@@ -19,9 +11,18 @@
 // no dups (removes edgecase (ordered))
 // target (integer) WILL exist in nums[i] (removes EC)
 
+// task:
 // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 // You must write an algorithm with O(log n) runtime complexity.
 
+// Pseudo:
+// binary search
+// is always just splitting in the middle
+// and then just search the left half of a group
+// then search the right half
+// but we cut the search in half
+
+// Approach:
 // Get length of integers array
 // divide that length by 2
 // take that as the middle point
@@ -30,7 +31,7 @@
 // compare if target is < or > than the midpoint
 // then find the next half of that midpoint
 // by dividing by half again
-// 
+//
 
 const searchInsert = () => {
 
