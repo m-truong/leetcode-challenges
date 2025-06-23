200f77798330b95dea68d4f6d3d8f05052e0fcc6
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-06-23 19:36:32 -0400

    Refactor comments for clarity and organization in the approach section

--- search-insert-position.js
+++ search-insert-position.js
@@ -17,15 +17,17 @@
 // but we cut the search in half
 
 // Approach:
+// Encapsulate bottom logic in a loop ===>
 // Get length of integers array
 // divide that length by 2
 // take that as the middle point
+// but make sure to round down using the modulus operator
 // compare if the target is less than or greater than half the total numbers of elements in the array
 // and then continue my search with the elements in the beginning half of the array
 // compare if target is < or > than the midpoint
 // then find the next half of that midpoint
 // by dividing by half again
-//
+// everything above ^^^ is encapsulated in a loop
 
 /**
  * search-insert-position
