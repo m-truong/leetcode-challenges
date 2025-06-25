067b0be697586304d75198f12a33bc64bfa4d62f
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-06-25 12:55:51 -0400

    Enhance documentation with detailed task description, constraints, and pseudo-code for searchInsert function

--- search-insert-position-v2.js
+++ search-insert-position-v2.js
@@ -1,3 +1,21 @@
+// Task:
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Constraints:
+// O(logn)
+// min [1] element in array
+// can have large neg. numbers
+// no dups (removes edgecase (ordered))
+// target (integer) WILL exist in nums[i] (removes EC)
+
+// Pseudo:
+// binary search
+// is always just splitting in the middle
+// and then just search the left half of a group
+// then search the right half
+// but we cut the search in half
+
 /**
  * @param {number[]} nums
  * @param {number} target
