e633a1d36eb431a154676eb39b6713ecaf9fc9d5
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-06-25 16:59:40 -0400

    Enhance comments in searchInsert function to clarify pointer usage and algorithm approach

--- search-insert-position-v3.js
+++ search-insert-position-v3.js
@@ -7,5 +7,10 @@
 
 // implement binary-search window algo
 var searchInsert = (nums, target) => {
-    // store two pointers
+    // store two pointers to both the first and last element
+    // we'll call them 'lowPointer' and 'highPointer'
+    let lo = 0, hi = nums.length; // tip: 'use inline multiple variables, but not very readable
+
+    // general approach/pseudo:
+    // to redetermine the mid-point between the low and high pointer variables indicating the ends of the integer array
 }
