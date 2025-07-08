50fa196b196f1055cd1f3424feeedd3c56b68d9c
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-07-07 21:33:51 -0400

    Enhance comments in calculate function for clarity on input expectations and reasoning behind stack approach

--- polish-notation-v2.js
+++ polish-notation-v2.js
@@ -6,5 +6,10 @@ exports.calculate = function(expression) {
     // the string expressions are an alternative notation for representing arthimetic to remove the need for (parentheses)
 
     // the expected output should be a numerical value (i.e. JavaScript primitive number-type)
-    //
+    // the logic needs to pass the tests that expect the same numerical value when evaluated using standard PEMDAS
+
+    // Reasoning:
+    // since this 'alternative' prefix notation of performing math relies on prefixing the numbers (i.e. operands) with their respective operators
+    // this requirement lends itself towards using a stack-approach implemented with a JS dynamic array
+
 };
