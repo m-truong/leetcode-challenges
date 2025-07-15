78302ba7cd65e6604beb2fe9ea08a349509587d3
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-07-15 17:28:54 -0400

    Add additional comments and constraints for clarity in insert-interval.js

--- insert-interval.js
+++ insert-interval.js
@@ -6,6 +6,21 @@
  * Note that you don't need to modify intervals in-place. You can make a new array and return it.
  */
 
+//  Q: counter questions:
+// 1. are the nested lists limited to 2 elements max? A: Yes
+// are start(i) and end(i) exclusively number primitive data types
+
+//  constraints:
+// length of outer list can be 0 (so edgecase of empty matrix)
+// max length is 10k inner arrays
+// max length of each individual array is 2 (i.e. a tuple)
+// the start(i) is used as the sorting for the rest of the elements in the nested array
+// the [newInterval] array has a length of 2
+
+// psuedo:
+// the key is to understand what 'overlaps' means in the context of the algo question
+// looking at the
+
 // javascript function declaration
 /**
  *
