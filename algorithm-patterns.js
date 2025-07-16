34b5807d67e9f1f696eff5cfed8b05821a2ef4ae
Michael Truong <59580072+m-truong@users.noreply.github.com>
2025-07-16 14:55:36 -0400

    Add algorithm patterns documentation in algorithm-patterns.js

---
+++ algorithm-patterns.js
@@ -0,0 +1,15 @@
+/**
+ * 1. Sliding Window
+ * useful for processing 'subarray/substrings' and calculating the 'maximum' or 'sum'
+ */
+
+/**
+ * 2. Two Pointers
+ * using two variables, and useful when comparing both elements at both ends of a sorted array
+ */
+
+/**
+ * 3. Fast & Slow Pointers
+ * useful for detecting cycles in a linked list
+ */
+
